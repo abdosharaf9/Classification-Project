ef41dc960af51df890560740b6cca27335d0096d
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-05-28 10:09:27 +0300

    Change open csv file method, shuffle dataset

---
+++ README.md
@@ -0,0 +1,4 @@
+# Classification Project
+
+## Dataset Link:
+https://www.kaggle.com/datasets/prosperchuks/health-dataset?select=hypertension_data.csv
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from KNN import *
 from PCA import *
-from dataset import *
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 """
-I = 12, gives acc 86 with our function
+# With the sorted dataset:-
 I = 9, gives acc 90 with built-in function
-"""
-# Ours
-# x_train, x_test, y_train, y_test = test_train_split(
-#     test_size= 0.2, random_state= 12
-# )
 
-# Built-in
+# With the shuffled dataset:-
+I = 46, gives acc 88 with built-in function
+"""
+# Sorted dataset
+# dataset = pd.read_csv("./hypertension_dataset.csv")
+
+# Shuffled dataset
+dataset = pd.read_csv("./shuffled_dataset.csv")
+
+features = np.array(dataset.drop("target", axis=1))
+classes = np.array(dataset["target"])
+
+# accs = []
+# for i in range(101):
+    # x_train, x_test, y_train, y_test = train_test_split(
+    #     features, classes, test_size= 0.2, random_state= i
+    # )
 x_train, x_test, y_train, y_test = train_test_split(
-    X, Y, test_size= 0.2, random_state= 9
+    features, classes, test_size= 0.2, random_state= 46
 )
 
 
@@ -28,8 +39,10 @@ knn = KNN(5)
 knn.fit(x_train_pca, y_train)
 y_pred = knn.predict(x_test_pca)
 print(accuracy_score(y_test, y_pred))
+    # accs.append(accuracy_score(y_test, y_pred))
 
-# print(f"Accuracy at i = {i} is: {accuracy_score(y_test, y_pred)}")
+# print(f"Max = {max(accs)}, I = {accs.index(max(accs))}")
+
 
 # knn = KNN(5)
 # knn.fit(x_train, y_train)
